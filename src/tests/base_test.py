6e177d4ae9f11177c08200c2ccf4180c2bfab733
Sandeep Thukral <sandeep.thukral@insided.com>
2020-01-17 15:06:40 +0100

    the most basic base tests files created

--- src/tests/base_test.py
+++ src/tests/base_test.py
@@ -2,4 +2,10 @@ import unittest
 
 
 class BaseTest(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        super().setUp()
+        # More will come here later
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        # More will come here later
--- src/tests/ui/base_ui_test.py
+++ src/tests/ui/base_ui_test.py
@@ -1,5 +1,26 @@
 from src.tests.base_test import BaseTest
-
+import src.tests.ui as tests_dir
+import os, inspect
 
 class BaseUiTest(BaseTest):
-    pass
+    def setUp(self) -> None:
+        super().setUp()
+        self.prepare_preconditions()
+        # More will come here later
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        # More will come here later
+
+    def prepare_preconditions(self) -> None:
+        pass
+
+    @property
+    def test_case_name(self) -> str:
+        return self.__class__.__name__
+
+    @property
+    def test_suite_name(self) -> str:
+        current_class_file_directory = os.path.dirname(inspect.getfile(self.__class__))
+        relative_directory = os.path.dirname(tests_dir.__file__)
+        return os.path.relpath(current_class_file_directory, relative_directory)
